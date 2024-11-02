8611ba7acd23f37f3a7d95a4f59d901f921fe65e
Nikhil Vyas <nvyas@ucsb.edu>
2024-11-02 15:06:09 -0700

    Added fixtures for menuItems

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,30 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneDate: {
+      id: 1,
+      diningCommonsCode: "ortega",
+      name: "pasta",
+      station: "entre",
+    },
+    threeDates: [
+      {
+        id: 1,
+        diningCommonsCode: "carillo",
+        name: "pizza",
+        station: "oven",
+      },
+      {
+        id: 2,
+        diningCommonsCode: "ortega",
+        name: "carrots",
+        station: "side",
+      },
+      {
+        id: 3,
+        diningCommonsCode: "portola",
+        name: "granola",
+        station: "salad",
+      },
+    ],
+  };
+  
+  export { ucsbDiningCommonsMenuItemFixtures };
