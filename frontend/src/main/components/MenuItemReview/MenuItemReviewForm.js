b840acb9b94ba67b1dc650b409cdf6a950cc02c2
Nilay Kundu <nilaykundu@ucsb.edu>
2024-11-03 20:55:56 -0800

    nk-MenuItemReview formatting

--- frontend/src/main/components/MenuItemReview/MenuItemReviewForm.js
+++ frontend/src/main/components/MenuItemReview/MenuItemReviewForm.js
@@ -66,7 +66,9 @@ function MenuItemReviewForm({
         </Col>
         <Col>
           <Form.Group className="mb-3">
-            <Form.Label htmlFor="dateReviewed">Date Reviewed (iso format)</Form.Label>
+            <Form.Label htmlFor="dateReviewed">
+              Date Reviewed (iso format)
+            </Form.Label>
             <Form.Control
               data-testid="MenuItemReviewForm-dateReviewed"
               id="dateReviewed"
--- frontend/src/tests/components/MenuItemReview/MenuItemReviewForm.test.js
+++ frontend/src/tests/components/MenuItemReview/MenuItemReviewForm.test.js
@@ -24,7 +24,9 @@ describe("MenuItemReviewForm tests", () => {
   test("renders correctly when passing in a MenuItemReview", async () => {
     render(
       <Router>
-        <MenuItemReviewForm initialContents={menuItemReviewFixtures.oneReview} />
+        <MenuItemReviewForm
+          initialContents={menuItemReviewFixtures.oneReview}
+        />
       </Router>,
     );
     await screen.findByTestId(/MenuItemReviewForm-id/);
@@ -40,7 +42,9 @@ describe("MenuItemReviewForm tests", () => {
     );
     await screen.findByTestId("MenuItemReviewForm-itemId");
     const itemIdField = screen.getByTestId("MenuItemReviewForm-itemId");
-    const dateReviwedField = screen.getByTestId("MenuItemReviewForm-dateReviewed");
+    const dateReviwedField = screen.getByTestId(
+      "MenuItemReviewForm-dateReviewed",
+    );
     const submitButton = screen.getByTestId("MenuItemReviewForm-submit");
 
     fireEvent.change(itemIdField, { target: { value: "bad-input" } });
@@ -79,14 +83,20 @@ describe("MenuItemReviewForm tests", () => {
     await screen.findByTestId("MenuItemReviewForm-itemId");
 
     const itemIdField = screen.getByTestId("MenuItemReviewForm-itemId");
-    const reviewerEmailField = screen.getByTestId("MenuItemReviewForm-reviewerEmail");
+    const reviewerEmailField = screen.getByTestId(
+      "MenuItemReviewForm-reviewerEmail",
+    );
     const starsField = screen.getByTestId("MenuItemReviewForm-stars");
-    const dateReviewedField = screen.getByTestId("MenuItemReviewForm-dateReviewed");
+    const dateReviewedField = screen.getByTestId(
+      "MenuItemReviewForm-dateReviewed",
+    );
     const commentsField = screen.getByTestId("MenuItemReviewForm-comments");
     const submitButton = screen.getByTestId("MenuItemReviewForm-submit");
 
     fireEvent.change(itemIdField, { target: { value: "20221" } });
-    fireEvent.change(reviewerEmailField, { target: { value: "test@example.com" } });
+    fireEvent.change(reviewerEmailField, {
+      target: { value: "test@example.com" },
+    });
     fireEvent.change(starsField, { target: { value: "5" } });
     // fireEvent.change(dateReviewedField, { target: { value: "20221" } });
     fireEvent.change(commentsField, { target: { value: "text" } });
