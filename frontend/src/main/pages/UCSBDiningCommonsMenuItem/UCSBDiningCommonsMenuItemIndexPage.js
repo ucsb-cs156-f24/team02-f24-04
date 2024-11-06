c104045cb1ca8fbc103a8d6287d0a64913e387b5
Nikhil Vyas <nvyas@ucsb.edu>
2024-11-06 14:55:28 -0800

    Added Index Page

--- frontend/src/main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemIndexPage.js
+++ frontend/src/main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemIndexPage.js
@@ -1,17 +1,45 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import React from "react";
+import { useBackend } from "main/utils/useBackend";
+import UCSBDiningCommonsMenuItemTable from "main/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemTable";
+import { Button } from "react-bootstrap";
+import { useCurrentUser, hasRole } from "main/utils/currentUser";
 
 export default function UCSBDiningCommonsMenuItemIndexPage() {
-  // Stryker disable all : UCSBDiningCommonsMenuItem for future implementation
+  
+  const currentUser = useCurrentUser();
+
+  const createButton = () => {
+    if (hasRole(currentUser, "ROLE_ADMIN")) {
+      return (
+        <Button
+          variant="primary"
+          href="/diningcommonsmenuitems/create"
+          style={{ float: "right" }}
+        >
+          Create UCSB Dining Commons Menu Item
+        </Button>
+      );
+    }
+  };
+
+  const {
+    data: menuItems,
+    error: _error,
+    status: _status,
+  } = useBackend(
+    // Stryker disable next-line all : don't test internal caching of React Query
+    ["/api/ucsbdiningcommonsmenuitem/all"],
+    { method: "GET", url: "/api/ucsbdiningcommonsmenuitem/all" },
+    [],
+  );
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p>
-          <a href="/diningcommonsmenuitems/create">Create</a>
-        </p>
-        <p>
-          <a href="/diningcommonsmenuitems/edit/1">Edit</a>
-        </p>
+      {createButton()}
+        <h1>UCSB Dining Commons Menu Item</h1>
+        <UCSBDiningCommonsMenuItemTable menuItems={menuItems} currentUser={currentUser} />
       </div>
     </BasicLayout>
   );
