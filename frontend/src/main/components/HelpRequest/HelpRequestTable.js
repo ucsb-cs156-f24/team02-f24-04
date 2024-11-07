fb819fdea09761d87bbbc78ef4d4d364df236ba7
Michael Hu <michael_hu@ucsb.edu>
2024-11-06 23:22:02 -0800

    yurr

--- frontend/src/main/components/HelpRequest/HelpRequestTable.js
+++ frontend/src/main/components/HelpRequest/HelpRequestTable.js
@@ -5,7 +5,7 @@ import { useBackendMutation } from "main/utils/useBackend";
 import {
   cellToAxiosParamsDelete,
   onDeleteSuccess,
-} from "main/utils/HelpRequestUtils"; // Make sure to define this in your utils
+} from "../../utils/HelpRequestUtils"; 
 import { useNavigate } from "react-router-dom";
 import { hasRole } from "main/utils/currentUser";
 
